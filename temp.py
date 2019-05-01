2f0f8b59b374391c0dcb4653374d33059cf82c4b
Egor Zhilenkov <rrrrkillerrrr@gmail.com>
2019-05-01 16:21:40 +0300

    First program

---
+++ temp.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+
+Это временный скриптовый файл.
+"""
+
+
+def f(x, y):
+    return(x + y)
+
+print(f(3, 4))
+
+def g(x, y):
+    return(x - y)
+
+print(g(5,2))
+
+print()
