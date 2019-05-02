de8119b4dfea55c81f53a1baea90efd227621004
Dmitry Zhilenkov <zhildemon@gmail.com>
2019-05-02 19:10:12 +1000

    Working plots

--- plotfunc.py
+++ plotfunc.py
@@ -8,11 +8,19 @@ Created on Thu May  2 11:18:55 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([1, 2, 3])
+x = np.linspace(-1, 10, 50)
 y = x**2 + 1
 
 print(x)
 print(y)
+
+plt.figure()
+
+plt.plot(x, y, "-ro")
+
+plt.show()
+
+y = np.sin(x)
 
 plt.figure()
 
